893f9ff44142527b53c1977d5dad25faf0e52a98
John Stitt <jmstitt@gmail.com>
2011-05-09 18:13:31 -0500

    Removed some very unnecessary comments from the header.

--- ChooseTastyBeverage.java
+++ ChooseTastyBeverage.java
@@ -1,20 +1,6 @@
 /*
-
-If you're reading this you got into my sourcecode, but anyway, here's the info:
-
-Program by: Sp0rus
-Date Written: December 5, 2009
+Program by: sp0rus
 Program Objective: use java's random number generator to decide what beverage (coffee or tea) to drink, and if tea, decide what type of tea.
-
------BEGIN GEEK CODE BLOCK-----
-Version 3.1
-GCS d-?pu s:(+) a--- C++(++++)>$ 
-ULC++()>$ P>$ L+(++)>$ !E W+++>$ 
-!N !o K-? w++++>$ !O !M !V PS---@ 
-PE++ !Y+ !PGP !t !5 X+ R(+) tv+ 
-b+++ DI+++ D++ G+ e h r++ y
-------END GEEK CODE BLOCK------
-
 */
 
 import java.util.Random;
